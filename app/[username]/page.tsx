09168e5822f67d8d6dfe049baec1e9ccd6f57e7f
sametcn99 <sametcn99@gmail.com>
2023-11-26 01:05:33 +0300

    sc

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -14,18 +14,28 @@ async function fetchUserPage(searchParams: any) {
     );
 
     if (!profileRes.ok) {
-      throw new Error(`Failed to fetch profile data: ${profileRes.statusText}`);
+      const errorText = await profileRes.text();
+      throw new Error(
+        `Failed to fetch profile data: ${profileRes.statusText}. ${errorText}`,
+      );
     }
 
     const profile = await profileRes.json();
     const data = profile.data;
 
-    return (
-      <section className="flex w-full flex-col justify-center space-y-4">
-        <Header profileData={data} />
-        <TabSwitcher username={username} />
-      </section>
-    );
+    if (data) {
+      return (
+        <section className="flex w-full flex-col justify-center space-y-4">
+          <Header profileData={data} />
+          <TabSwitcher username={username} />
+        </section>
+      );
+    } else {
+      console.log(data);
+      return (
+        <div>Error fetching user profile. Please try again later.{data}\</div>
+      );
+    }
   } catch (error) {
     console.error("Error fetching user profile:", error);
     return <div>Error fetching user profile. Please try again later.</div>;
