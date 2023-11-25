27e5928bfdd83dc54c4260375c8fa5c0d7e25b6b
sametcn99 <sametcn99@gmail.com>
2023-11-25 05:26:47 +0300

    working correct

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -1,47 +1,38 @@
+// fetchUserPage.js
+
+import React from "react";
 import Header from "@/components/card-components/Header";
 import TabSwitcher from "@/components/tab-switcher";
 import { getBaseUrl } from "@/utils/utils";
 
-// UserPage component
-async function UserPage(searchParams: any) {
-  const username = searchParams.params.username;
-  const profileRes = await fetch(
-    `${getBaseUrl()}/api/profile?username=${username}`,
-  );
-  const profile = await profileRes.json();
-  const data = profile.data;
-
-  if (profile.data) {
-    return (
-      <section className=" flex w-full flex-col justify-center space-y-4">
-        <Header
-          login={data.login}
-          created_at={data.created_at}
-          updated_at={data.updated_at}
-          avatar_url={data.avatar_url}
-          gravatar_url={data.gravatar_url}
-          html_url={data.html_url}
-          name={data.name}
-          bio={data.bio}
-          company={data.company}
-          location={data.location}
-          email={data.email}
-          followers={data.followers}
-          following={data.following}
-          public_repos={data.public_repos}
-          public_gists={data.public_gists}
-          twitter_username={data.twitter_username}
-          type={data.type}
-          followers_url={data.followers_url}
-          following_url={data.following_url}
-          gists_url={data.gists_url}
-        />
-        <TabSwitcher username={username} />
-      </section>
+async function fetchUserPage(searchParams: any) {
+  try {
+    const username = searchParams.params.username;
+    const profileRes = await fetch(
+      `${getBaseUrl()}/api/profile?username=${username}`,
     );
-  } else {
-    return null;
+
+    if (!profileRes.ok) {
+      throw new Error(`Failed to fetch profile data: ${profileRes.statusText}`);
+    }
+
+    const profile = await profileRes.json();
+    const data = profile.data;
+
+    if (data) {
+      return (
+        <section className="flex w-full flex-col justify-center space-y-4">
+          <Header profileData={data} />
+          <TabSwitcher username={username} />
+        </section>
+      );
+    } else {
+      return <div>Error fetching user profile. Please try again later.</div>;
+    }
+  } catch (error) {
+    console.error("Error fetching user profile:", error);
+    return <div>Error fetching user profile. Please try again later.</div>;
   }
 }
 
-export default UserPage;
+export default fetchUserPage;
--- components/card-components/Header.tsx
+++ components/card-components/Header.tsx
@@ -10,69 +10,69 @@ import Avatar from "./Avatar";
 import ModalComponent from "../modal-component";
 import { getBaseUrl } from "@/utils/utils";
 
-export default function Header({ ...props }) {
+export default function Header({ profileData }: any) {
   return (
     <section className="flex  items-center justify-center  ">
       <Card className="flex flex-col items-center justify-center p-5 md:flex-row">
         <Card className="scale-80 md:h-fit md:w-fit">
           <Avatar
-            avatar_url={props.avatar_url}
-            gravatar_url={props.gravatar_url}
+            avatar_url={profileData.avatar_url}
+            gravatar_url={profileData.gravatar_url}
           />
         </Card>
         <section className="flex h-fit w-full max-w-[35rem] flex-col items-start justify-center p-2">
           <span className="flex flex-col">
             <span className="text-5xl font-bold" title="name">
-              {props.name}
+              {profileData.name}
             </span>
             <span className="font-extralight" title="username">
-              @{props.login}
+              @{profileData.login}
             </span>
             <Divider className="my-1" />
           </span>
           <span className="break-words" title="biography">
-            {props.bio}
+            {profileData.bio}
           </span>
           <span className="break-words" title="email">
-            {props.email}
+            {profileData.email}
           </span>
           <span className="break-words" title="company">
-            {props.company}
+            {profileData.company}
           </span>
           <span className="break-words" title="location">
-            From: {props.location}
+            From: {profileData.location}
           </span>
           <span title="user profile type">
-            Profile Type: <span>{props.type}</span>
+            Profile Type: <span>{profileData.type}</span>
           </span>
           <span className="break-words" title="twitter user name">
-            {props.twitter_username}
+            {profileData.twitter_username}
           </span>
           <ModalComponent
-            title={`Followers: ${props.followers}`}
+            title={`Followers: ${profileData.followers}`}
             modalTitle="Followers"
-            url={`${getBaseUrl()}/api/followers?username=${props.login}`}
+            url={`${getBaseUrl()}/api/followers?username=${profileData.login}`}
           />
           <ModalComponent
-            title={`Followings: ${props.following}`}
+            title={`Followings: ${profileData.following}`}
             modalTitle="Followings"
-            url={`${getBaseUrl()}/api/following?username=${props.login}`}
+            url={`${getBaseUrl()}/api/following?username=${profileData.login}`}
           />
           <span
             className="w-fit"
             title="Public Repositories"
-          >{`Public Repos: ${props.public_repos}`}</span>
+          >{`Public Repos: ${profileData.public_repos}`}</span>
           <span
             className="w-fit"
             title="Public Gists"
-          >{`Public Gists: ${props.public_gists}`}</span>
+          >{`Public Gists: ${profileData.public_gists}`}</span>
           <span className="break-words" title="Account Created at">
             Member Since: <br />
-            {new Date(props.created_at).toLocaleString()}
+            {new Date(profileData.created_at).toLocaleString()}
           </span>
           <span title="Last Profile Update">
             Last Profile Update: <br />
-            <span>{new Date(props.updated_at).toLocaleString()}</span>
+            <span>{new Date(profileData.updated_at).toLocaleString()}</span>
           </span>
         </section>
       </Card>
