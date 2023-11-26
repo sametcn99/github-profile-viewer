981c78612af6b9fededa244bb0506c99e1d715da
sametcn99 <sametcn99@gmail.com>
2023-11-26 05:11:53 +0300

    production

--- utils/utils.ts
+++ utils/utils.ts
@@ -9,7 +9,7 @@ export const getSiteUrl = () => {
 
   // Define the base URL for both production and local development environments.
   const baseUrl = isProduction
-    ? "https://github-profile-next-ui.vercel.app"
+    ? "https://github-profile-viewer-next-ui.vercel.app/"
     : "http://localhost:3000";
 
   // Return the appropriate base URL based on the environment.
