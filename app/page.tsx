da83bd727af3d362253673876c66207dc286e706
samet <sametcn99@gmail.com>
2024-01-22 03:40:02 +0300

    add chrome extension banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Section, Text } from "@radix-ui/themes";
 import Rate from "../components/Rate";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <Section className="flex flex-col items-center justify-center gap-4 break-words  py-8 text-center md:py-10">
+      <Section className="flex flex-col items-center justify-center gap-4 break-words py-8 text-center md:py-10">
         <Text className="text-3xl font-bold">Github Profile Viewer</Text>
         <Text className="max-w-[50rem]">
           Welcome to the Github Profile Viewer, a dynamic web platform powered
@@ -14,6 +16,18 @@ export default function Home() {
           to the open-source community.
         </Text>
         <Rate />
+        <Link
+          href="https://chromewebstore.google.com/detail/gpv-opener/abgechjdbcnlcdcmhkaakobeoimjgkmb"
+          target="_blank"
+        >
+          <Image
+            width={180}
+            height={40}
+            src="/icons/chrome-extension-dark.png"
+            alt="GPV Opener"
+            className="shadow-md shadow-gray-700 transition-all duration-500 hover:scale-105"
+          />
+        </Link>
       </Section>
     </>
   );
