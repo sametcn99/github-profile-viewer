db08488379c9d50808b040b304177f714b1d8039
samet <sametcn99@gmail.com>
2024-02-18 21:16:32 +0300

    features section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import ChromeStore from "@/components/home/ChromeStore";
 import Features from "@/components/home/Features";
 import Hero from "@/components/home/Hero";
-import TechStack from "@/components/home/TechStack";
 
 export default function Home() {
   return (
