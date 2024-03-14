421a78989e2536d0dd1260465cfd36a50a53de13
samet <sametcn99@gmail.com>
2024-03-14 21:05:13 +0300

    animated header

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -8,10 +8,7 @@ export default function Hero() {
       className="flex flex-col items-center justify-center gap-4 text-center"
       id="hero"
     >
-      <Heading
-        size="9"
-        className="bg-gradient-to-r from-blue-600 via-green-600 to-purple-600 bg-clip-text text-transparent"
-      >
+      <Heading size="9" className="animated-header">
         Github Profile Viewer
       </Heading>
       <Text className="max-w-[50rem]">
